dbf143fab784f54f1fd2e0001354668f9f2a8961
Robert Wiebe <robert.wiebe9@icloud.com>
2021-05-10 15:44:47 +0200

    Initial Commit

---
+++ Quiz/QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApp.swift
+//  Quiz
+//
+//  Created by Robert Wiebe on 10.05.21.
+//
+
+import SwiftUI
+
+@main
+struct QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
